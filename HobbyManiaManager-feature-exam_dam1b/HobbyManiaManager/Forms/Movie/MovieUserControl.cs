186b914347014c7222612ca732b8e59f81b6b131
Andrés Cardona <andrescardonasst@gmail.com>
2025-05-13 17:06:46 +0200

    Se ha añanido el nombre el usuario quien tiene alquilado la pelicula

--- HobbyManiaManager-feature-exam_dam1b/HobbyManiaManager/Forms/Movie/MovieUserControl.cs
+++ HobbyManiaManager-feature-exam_dam1b/HobbyManiaManager/Forms/Movie/MovieUserControl.cs
@@ -11,15 +11,19 @@ namespace HobbyManiaManager
     public partial class MovieUserControl : UserControl
     {
         private CultureInfo cultureInfo;
-        private RentalService service;
+        private RentalService _service;
         private Movie Movie;
+        private CustomersRepository _customersRepository;
+
         public Action _refreshAction;
 
         public MovieUserControl()
         {
             InitializeComponent();
             this.cultureInfo = new CultureInfo("es-ES");
-            this.service = new RentalService();
+            this._service = new RentalService();
+            this._customersRepository = CustomersRepository.Instance;
+
         }
 
         public void Load(Movie movie)
@@ -59,7 +63,7 @@ namespace HobbyManiaManager
 
         private void CheckAvailability(Movie movie)
         {
-            bool available = service.IsAvailable(movie);
+            bool available = _service.IsAvailable(movie);
             if (available)
             {
                 this.pictureBoxAvailable.BackColor = Color.Green;
@@ -69,9 +73,12 @@ namespace HobbyManiaManager
             }
             else
             {
+                var rental = _service.GetMovieRental(movie.Id);
+                var customer = _customersRepository.GetById(rental.CustomerId);
                 this.buttonStartEndRent.Text = "End Rent";
                 this.pictureBoxAvailable.BackColor = Color.Red;
-                this.labelAvailable.Text = "Rental not available";
+                this.labelAvailable.Text = $"Not available. Rented by: {customer.Name}({customer.Id})";
+                
             }
         }
 
